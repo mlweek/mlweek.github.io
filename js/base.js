bfc02cd8f12d8c51ece8220496dba6c86f8a01e5
Rossi Oddet <rossi.oddet@gmail.com>
2015-12-12 16:51:36 +0100

    page lieu

--- js/base.js
+++ js/base.js
@@ -174,7 +174,7 @@ var wplook = (function ($) {
 				};
 
 				map = new google.maps.Map(document.getElementById('js-map'),mapOptions);
-				var image = '/images/map-marker.png';
+				var image = '/img/map-marker.png';
 				var myLatLng = new google.maps.LatLng(wplMarker.Lat, wplMarker.Long);
 				var beachMarker = new google.maps.Marker({
 					position: myLatLng,
